9d133b4f1267c3308ed24415b234f8b4471ed75e
s2cl <yukisunictf@gmail.com>
2020-12-09 18:24:24 +0900

    Add: tokenの設定

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,13 @@ INSTALLED_APPS = [
     'django_boost'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
