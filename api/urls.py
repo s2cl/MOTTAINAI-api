2c5e7c690cbd1e07c36702eca5196c4507b9f390
s2cl <yukisunictf@gmail.com>
2020-12-09 19:04:59 +0900

    Add: urlの設定

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,13 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
+router.register(r'items', views.ItemViewSet)
+router.register(r'master', views.MasterViewSet)
+router.register(r'list', views.ShoppingListViewSet)
+router.register(r'listitem', views.ShoppingItemViewSet)
+
 urlpatterns = [
+    path('register/', views.UserRegisterView.as_view()),
+    path('user/', views.UserView.as_view()),
     path('', include(router.urls)),
 ]
