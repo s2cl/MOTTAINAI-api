2c036839488c657a00c3b96e5f56d6a35dd938f0
s2cl <yukisunictf@gmail.com>
2020-12-09 19:06:01 +0900

    Add: token作成とドキュメント

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 from rest_framework.authtoken import views
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('token/', views.obtain_auth_token),
+    path('docs/', include_docs_urls(title='Omotenashi App Documentation')),
     path('', include('api.urls'))
 ]
