9855d14d348b7ea57946bbfce7b6fef9680db128
s2cl <yukisunictf@gmail.com>
2020-12-09 19:03:58 +0900

    Add: 管理者ページ作成

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+from django_boost.admin import register_all
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
+register_all(models)
